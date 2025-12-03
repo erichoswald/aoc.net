37bb3017a5e484ad5bf34cddf9599daaa1f8caa8
Erich Oswald <erich.oswald@ergon.ch>
2025-12-03 23:07:11 +0100

    Day03 part 2

--- 2025/Solutions/Day03.fs
+++ 2025/Solutions/Day03.fs
@@ -1,21 +1,32 @@
 namespace _2025.Solutions
 
 module Day03 =
-    let largestJoltage (bank: string) =
-        bank
-        |> Seq.map (fun c -> int c - int '0')
-        |> Seq.fold
-            (fun (leadingDigit, maxJoltage) battery ->
-                let joltage = 10 * leadingDigit + battery
-                if battery > leadingDigit then
-                    battery, max maxJoltage joltage
-                else
-                    leadingDigit, max maxJoltage joltage)
-            (0, 0)
-        |> snd
+    let rec weakestIndex selection index next =
+        match selection with
+        | [] -> None // Not expected except in test.
+        | [ last ] when last < next -> Some index // Replace last value.
+        | a :: b :: _ when a < b -> Some index // Replace the first value whose successor is higher in value.
+        | _ :: tail -> weakestIndex tail (index + 1) next // Keep looking.
 
-    let part1 (banks: string list) = banks |> Seq.sumBy largestJoltage
+    let maximize selection battery =
+        match weakestIndex selection 0 battery with
+        | None -> selection
+        | Some i -> selection[0 .. i - 1] @ selection[i + 1 ..] @ [ battery ]
+
+    let joltage selection =
+        selection |> List.fold (fun acc digit -> 10L * acc + digit) 0L
+
+    let largestJoltage n bank =
+        bank
+        |> Seq.map (fun c -> int64 c - int64 '0')
+        |> Seq.fold maximize (List.replicate n 0L)
+        |> joltage
+
+    let part1 (banks: string list) = banks |> Seq.sumBy (largestJoltage 2)
+   
+    let part2 (banks: string list) = banks |> Seq.sumBy (largestJoltage 12)
 
     let run =
         let input = System.IO.File.ReadAllLines "2025/Inputs/03/input.txt" |> List.ofSeq
         printfn $"Day 03 - Part 1: {part1 input}"
+        printfn $"Day 03 - Part 2: {part2 input}"
--- 2025/Tests/Day03Tests.fs
+++ 2025/Tests/Day03Tests.fs
@@ -4,22 +4,65 @@ open Expecto
 open _2025.Solutions
 
 let sampleLines = [
-    "987654321111111";
-    "811111111111119";
-    "234234234234278";
-    "818181911112111";
+    "987654321111111"
+    "811111111111119"
+    "234234234234278"
+    "818181911112111"
 ]
 
 [<Tests>]
 let tests =
     testList "Day 03 Tests" [
         testTheory
-            "largestJoltage returns maximum reachable joltage"
-            [0, 98; 1, 89; 2, 78; 3, 92]
-        <| fun (index, joltage) ->
-            Expect.equal (Day03.largestJoltage (sampleLines[index])) joltage $"Expected %d{joltage}"
+            "weakestIndex returns index of weakest element"
+            [
+                [ ], 0L, None;
+                [ 3L ], 2L, None;
+                [ 1L ], 2L, Some 0;
+                [ 1L; 2L ], 2L, Some 0;
+                [ 3L; 4L; 5L ], 1L, Some 0;
+                [ 4L; 3L; 2L; 3L; 4L ], 3L, Some 2;
+            ]
+            <| fun (selection, next, expected) ->
+                let actual = Day03.weakestIndex selection 0 next
+                Expect.equal actual expected $"Expected weakest index of %A{selection} with %d{next} to be different"
+
+        testCase "maximize replaces the weakest element if it exists" <| fun _ ->
+            let input = [ 0L; 4L; 5L; 2L; 8L ]
+            let result = Day03.maximize input 7L
+            Expect.equal result [ 4L; 5L; 2L; 8L; 7L ] "Expected maximize to replace the weakest element"
+
+        testCase "maximize has no effect if there is no element weaker than the next candidate" <| fun _ ->
+            let input = [ 9L; 9L ]
+            let result = Day03.maximize input 1L
+            Expect.equal result [ 9L; 9L ] "Expected maximize to have no effect"
+
+        testCase "joltage computes the resulting joltage" <| fun _ ->
+            Expect.equal (Day03.joltage [ 0L; 1L; 2L; 3L ]) 123L "Expected joltage to accumulate digit values"
+
+        testTheory
+            "largestJoltage 2 returns the maximum reachable joltage"
+            [ 0, 98; 1, 89; 2, 78; 3, 92 ]
+            <| fun (index, joltage) ->
+                Expect.equal
+                    (Day03.largestJoltage 2 (sampleLines[index]))
+                    joltage
+                    $"Expected a largest joltage of %d{joltage}"
+
+        testTheory
+            "largestJoltage 12 returns maximum reachable joltage"
+            [ 0, 987654321111L; 1, 811111111119L; 2, 434234234278L; 3, 888911112111L ]
+            <| fun (index, joltage) ->
+                Expect.equal
+                    (Day03.largestJoltage 12 (sampleLines[index]))
+                    joltage
+                    $"Expected a largest joltage of %d{joltage}"
 
         testCase "Part 1" <| fun _ ->
-            let expected = 357
-            Expect.equal (Day03.part1 sampleLines) expected $"Expected %d{expected}"
+              let expected = 357
+              Expect.equal (Day03.part1 sampleLines) expected "Wrong result for part 1"
+
+        testCase "Part 2" <| fun _ ->
+              let expected = 3121910778619L
+              Expect.equal (Day03.part2 sampleLines) expected "Wrong result for part 2"
     ]
