d67938fcf61021bf1faf78c7ccb66e526c27ca53
Erich Oswald <erich.oswald.privat@gmail.com>
2025-12-04 11:47:44 +0100

    Day 04 part 2

--- 2025/Solutions/Day04.fs
+++ 2025/Solutions/Day04.fs
@@ -1,31 +1,58 @@
 namespace _2025.Solutions
 
 module Day04 =
-    let Roll = '@'
-
-    let isInGrid (grid: string array) row col =
-        row >= 0 && row < grid.Length && col >= 0 && col < grid[row].Length
-
-    let countAdjacentRolls (grid: string array) row col =
-        let mutable count = 0
-        for r in row-1 .. row+1 do
-            for c in col-1 .. col+1 do
-                if not (r = row && c = col) && isInGrid grid r c && grid[r][c] = Roll then count <- count + 1
-        count
-
-    let countAccessibleRolls (grid: string array) =
-        let mutable count = 0
-        for row in 0 .. grid.Length-1 do
-            for col in 0 .. grid[row].Length-1 do
-                if grid[row][col] = Roll then
-                    let adjacentRollCount = countAdjacentRolls grid row col
-                    if adjacentRollCount < 4 then
-                        count <- count + 1
-        count
-
     let parseInput file =
         System.IO.File.ReadAllLines file
+        |> Array.map _.ToCharArray()
+        |> array2D
+
+    let isInGrid (grid: char array2d) row col =
+        row >= 0 && row < Array2D.length1 grid && col >= 0 && col < Array2D.length2 grid
+    
+    let isRoll (grid: char array2d) row col =
+        grid[row, col] = '@'
+
+    let coords top bottom left right = [
+        for row in top .. bottom do
+            for col in left .. right do
+                yield row, col
+    ]
+
+    let countAdjacentRolls (grid: char array2d) row col =
+        let mutable count = 0
+        for r, c in coords (row - 1) (row + 1) (col - 1) (col + 1) do
+            if not (r = row && c = col) && isInGrid grid r c && isRoll grid r c then count <- count + 1
+        count
+
+    let isAccessibleRoll (grid: char array2d) row col =
+        countAdjacentRolls grid row col < 4
+
+    let listAccessibleRolls (grid: char array2d) =
+        let rows = Array2D.length1 grid
+        let cols = Array2D.length2 grid
+        let mutable rolls = []
+        for row, col in coords 0 (rows - 1) 0 (cols - 1) do
+            if isRoll grid row col && isAccessibleRoll grid row col then
+                rolls <- (row, col) :: rolls
+        rolls
+
+    let countAccessibleRolls (grid: char array2d) =
+        (listAccessibleRolls grid).Length
+
+    let removeRolls (grid: char array2d) (rolls: (int * int) list) =
+        for row, col in rolls do
+            grid[row, col] <- '.'
+
+    let removeAllAccessibleRolls (grid: char array2d) =
+        let mutable count = 0
+        let mutable accessibleRolls = listAccessibleRolls grid
+        while accessibleRolls.Length > 0 do
+            count <- count + accessibleRolls.Length
+            removeRolls grid accessibleRolls
+            accessibleRolls <- listAccessibleRolls grid
+        count
 
     let run =
         let grid = parseInput "2025/Inputs/04/input.txt"
-        printf $"Day 04 - Part 1: {countAccessibleRolls grid}"
+        printfn $"Day 04 - Part 1: {countAccessibleRolls grid}"
+        printfn $"Day 04 - Part 2: {removeAllAccessibleRolls grid}"
--- 2025/Tests/Day04Tests.fs
+++ 2025/Tests/Day04Tests.fs
@@ -7,19 +7,24 @@ open _2025.Solutions
 let tests =
     testList "Day 04 Tests" [
         testCase "countAdjacentRolls on single cell" <| fun _ ->
-            let input = [| "@" |]
+            let input = array2D [| "@" |]
             Expect.equal (Day04.countAdjacentRolls input 0 0) 0 "Unexpected number of adjacent rolls"
 
         testCase "countAdjacentRolls on full grid" <| fun _ ->
-            let input = [| "@@@"; "@@@"; "@@@" |]
+            let input = array2D [| "@@@"; "@@@"; "@@@" |]
             Expect.equal (Day04.countAdjacentRolls input 1 1) 8 "Unexpected number of adjacent rolls"
 
         testCase "countAdjacentRolls on partially full grid" <| fun _ ->
-            let input = [| "@.@"; "@@."; ".@@" |]
+            let input = array2D [| "@.@"; "@@."; ".@@" |]
             Expect.equal (Day04.countAdjacentRolls input 1 1) 5 "Unexpected number of adjacent rolls"
         
         testCase "part 1 computes correct result on sample input" <| fun _ ->
             let grid = Day04.parseInput "2025/Samples/04/sample.txt"
             let result = Day04.countAccessibleRolls grid
             Expect.equal result 13 "Unexpected result for part 1"
+        
+        testCase "part 2 computes correct result on sample input" <| fun _ ->
+            let grid = Day04.parseInput "2025/Samples/04/sample.txt"
+            let result = Day04.removeAllAccessibleRolls grid
+            Expect.equal result 43 "Unexpected result for part 2"
     ]
