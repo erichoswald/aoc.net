2dbf971d78786c1a766e861b021823e71f2b39f9
Erich Oswald <erich.oswald.privat@gmail.com>
2025-12-04 12:42:03 +0100

    Day 03: Improve solution with more efficient algorithm

--- 2025/Solutions/Day03.fs
+++ 2025/Solutions/Day03.fs
@@ -1,32 +1,32 @@
 namespace _2025.Solutions
 
 module Day03 =
-    let rec weakestIndex selection index next =
-        match selection with
-        | [] -> None // Not expected except in test.
-        | [ last ] when last < next -> Some index // Replace last value.
-        | a :: b :: _ when a < b -> Some index // Replace the first value whose successor is higher in value.
-        | _ :: tail -> weakestIndex tail (index + 1) next // Keep looking.
+    let rec traverseAvailableBatteries batteryCount (available: int64 list) joltage : int64 =
+        if batteryCount = 0 then
+            // The number of requested batteries has been provided.
+            joltage
+        else
+            // Choose the first battery with the highest joltage that leaves enough remaining batteries available.
+            let index, max =
+                available[.. available.Length - batteryCount]
+                |> List.mapi (fun index value -> index, value)
+                |> List.maxBy snd
 
-    let maximize selection battery =
-        match weakestIndex selection 0 battery with
-        | None -> selection
-        | Some i -> selection[0 .. i - 1] @ selection[i + 1 ..] @ [ battery ]
+            // Continue with the remaining batteries.
+            traverseAvailableBatteries (batteryCount - 1) available[index + 1 ..] (joltage * 10L + max)
 
-    let joltage selection =
-        selection |> List.fold (fun acc digit -> 10L * acc + digit) 0L
+    let maximumJoltage batteryCount (bank: char seq)=
+        let batteries = bank |> Seq.toList |> List.map (fun c -> int64 c - int64 '0')
+        traverseAvailableBatteries batteryCount batteries 0L
 
-    let largestJoltage n bank =
-        bank
-        |> Seq.map (fun c -> int64 c - int64 '0')
-        |> Seq.fold maximize (List.replicate n 0L)
-        |> joltage
-
-    let part1 (banks: string list) = banks |> Seq.sumBy (largestJoltage 2)
+    let totalMaximumJoltage batteryCount (banks: string list) =
+        banks |> Seq.sumBy (maximumJoltage batteryCount)
+    
+    let part1 (banks: string list) = totalMaximumJoltage 2 banks
    
-    let part2 (banks: string list) = banks |> Seq.sumBy (largestJoltage 12)
+    let part2 (banks: string list) = totalMaximumJoltage 12 banks
 
     let run =
-        let input = System.IO.File.ReadAllLines "2025/Inputs/03/input.txt" |> List.ofSeq
-        printfn $"Day 03 - Part 1: {part1 input}"
-        printfn $"Day 03 - Part 2: {part2 input}"
+        let banks = System.IO.File.ReadAllLines "2025/Inputs/03/input.txt" |> List.ofSeq
+        printfn $"Day 03 - Part 1: {part1 banks}"
+        printfn $"Day 03 - Part 2: {part2 banks}"
--- 2025/Tests/Day03Tests.fs
+++ 2025/Tests/Day03Tests.fs
@@ -14,47 +14,20 @@ let sampleLines = [
 let tests =
     testList "Day 03 Tests" [
         testTheory
-            "weakestIndex returns index of weakest element"
-            [
-                [ ], 0L, None;
-                [ 3L ], 2L, None;
-                [ 1L ], 2L, Some 0;
-                [ 1L; 2L ], 2L, Some 0;
-                [ 3L; 4L; 5L ], 1L, Some 0;
-                [ 4L; 3L; 2L; 3L; 4L ], 3L, Some 2;
-            ]
-            <| fun (selection, next, expected) ->
-                let actual = Day03.weakestIndex selection 0 next
-                Expect.equal actual expected $"Expected weakest index of %A{selection} with %d{next} to be different"
-
-        testCase "maximize replaces the weakest element if it exists" <| fun _ ->
-            let input = [ 0L; 4L; 5L; 2L; 8L ]
-            let result = Day03.maximize input 7L
-            Expect.equal result [ 4L; 5L; 2L; 8L; 7L ] "Expected maximize to replace the weakest element"
-
-        testCase "maximize has no effect if there is no element weaker than the next candidate" <| fun _ ->
-            let input = [ 9L; 9L ]
-            let result = Day03.maximize input 1L
-            Expect.equal result [ 9L; 9L ] "Expected maximize to have no effect"
-
-        testCase "joltage computes the resulting joltage" <| fun _ ->
-            Expect.equal (Day03.joltage [ 0L; 1L; 2L; 3L ]) 123L "Expected joltage to accumulate digit values"
-
-        testTheory
-            "largestJoltage 2 returns the maximum reachable joltage"
+            "maximumJoltage 2 returns the maximum reachable joltage when choosing 2 batteries"
             [ 0, 98; 1, 89; 2, 78; 3, 92 ]
             <| fun (index, joltage) ->
                 Expect.equal
-                    (Day03.largestJoltage 2 (sampleLines[index]))
+                    (Day03.maximumJoltage 2 sampleLines[index])
                     joltage
                     $"Expected a largest joltage of %d{joltage}"
 
         testTheory
-            "largestJoltage 12 returns maximum reachable joltage"
+            "maximumJoltage 12 returns maximum reachable joltage when choosing 12 batteries"
             [ 0, 987654321111L; 1, 811111111119L; 2, 434234234278L; 3, 888911112111L ]
             <| fun (index, joltage) ->
                 Expect.equal
-                    (Day03.largestJoltage 12 (sampleLines[index]))
+                    (Day03.maximumJoltage 12 sampleLines[index])
                     joltage
                     $"Expected a largest joltage of %d{joltage}"
 
