c155076b8e88c21a3e34fb87ae8324827788203d
Erich Oswald <erich.oswald.privat@gmail.com>
2025-12-12 17:57:24 +0100

    Day 11 part 2

--- 2025/Solutions/Day11.fs
+++ 2025/Solutions/Day11.fs
@@ -3,29 +3,50 @@ namespace _2025.Solutions
 module Day11 =
     type Device = {
         Name: string
-        Outputs: string list
+        Outputs: string[]
     }
+    type Rack = Map<string, string[]>
 
     let parseDevice (line: string) : Device =
         let parts = line.Split(':')
         let outputs = parts[1].Split(' ', System.StringSplitOptions.RemoveEmptyEntries)
-        { Name = parts[0]; Outputs = outputs |> List.ofArray }
+        { Name = parts[0]; Outputs = outputs }
 
-    let parseDevices (lines: string seq) =
-        lines |> Seq.map parseDevice |> List.ofSeq
+    let parseRack (lines: string seq) : Rack =
+        lines
+        |> Seq.map parseDevice
+        |> Seq.map (fun device -> device.Name, device.Outputs )
+        |> Map.ofSeq
 
-    let countPaths (devices: Device list) =
-        let rack = devices |> List.map (fun d -> d.Name, d.Outputs) |> Map.ofList
-        
+    let countPaths (rack: Rack) (origin: string) (destination: string) =
+        let cache = System.Collections.Generic.Dictionary<string, int64>()
+
         let rec countPath (name: string) =
-            if name = "out" then
-                1
-            else
-                rack[name] |> List.sumBy countPath
+            if not (cache.ContainsKey(name)) then
+                let pathCount =
+                    if name = destination then 1L
+                    elif rack.ContainsKey(name) then rack[name] |> Array.sumBy countPath
+                    else 0L
+                cache.Add(name, pathCount)
+            cache[name]
         
-        countPath "you"
+        countPath origin
+    
+    let part2 (rack: Rack) =
+        let fftToDacCount = countPaths rack "fft" "dac"
+        if fftToDacCount > 0 then
+            let svrToFftCount = countPaths rack "svr" "fft"
+            let dacToOutCount = countPaths rack "dac" "out"
+            svrToFftCount * fftToDacCount * dacToOutCount
+        else
+            let svrToDacCount = countPaths rack "svr" "dac"
+            let dacToFftCount = countPaths rack "dac" "fft"
+            let fftToOutCount = countPaths rack "fft" "out"
+            svrToDacCount * dacToFftCount * fftToOutCount
 
     let run =
-        let devices = parseDevices (System.IO.File.ReadLines "2025/Inputs/11/input.txt")
-        let result, millis = Measure.measure countPaths devices
+        let rack = parseRack (System.IO.File.ReadLines "2025/Inputs/11/input.txt")
+        let result, millis = Measure.measure (fun () -> countPaths rack "you" "out")
         printfn $"Day 11 - Part 1: %d{result} (took %.3f{millis}ms)"
+        let result, millis = Measure.measure (fun () -> part2 rack)
+        printfn $"Day 11 - Part 2: %d{result} (took %.3f{millis}ms)"
--- 2025/Tests/Day11Tests.fs
+++ 2025/Tests/Day11Tests.fs
@@ -5,15 +5,20 @@ open _2025.Solutions
 
 [<Tests>]
 let tests =
-    let sample1Devices = Day11.parseDevices (System.IO.File.ReadAllLines("2025/Samples/11/sample1.txt"))
+    let sample1Rack = Day11.parseRack (System.IO.File.ReadAllLines("2025/Samples/11/sample1.txt"))
+    let sample2Rack = Day11.parseRack (System.IO.File.ReadAllLines("2025/Samples/11/sample2.txt"))
 
     testList "Day 11 Tests" [
         testCase "parseDevice parses device correctly" <| fun _ ->
             let result = Day11.parseDevice "aaa: you hhh"
-            let expected: Day11.Device = { Name = "aaa"; Outputs = ["you"; "hhh"] }
+            let expected: Day11.Device = { Name = "aaa"; Outputs = [|"you"; "hhh"|] }
             Expect.equal result expected "Failed to parse device"
         
         testCase "countPaths counts sample paths correctly" <| fun _ ->
-            let result = Day11.countPaths sample1Devices
+            let result = Day11.countPaths sample1Rack "you" "out"
             Expect.equal result 5 "Failed to count paths"
+        
+        testCase "part2 counts sample paths correctly" <| fun _ ->
+            let result = Day11.part2 sample2Rack
+            Expect.equal result 2 "Failed to count paths through bottleneck nodes"
     ]
